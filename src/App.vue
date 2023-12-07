09d14c2feedc1df6a4e04a189bbd5f619ce02f24
kassahun <kassuhan@gmail.com>
2023-12-07 09:31:00 +0200

    files modified and added

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div  id="app">
     <h1>To-Do List</h1>
     <to-do-form @todo-added="addToDo"></to-do-form>
-     <ul>
+    <h2 id="list-summary" ref="listSummary" tabindex="-1">{{ listSummary }}</h2>
+     <ul :aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item  
           :label="item.label" 
@@ -33,12 +34,20 @@ export default {
         {id:uniqueId('todo-'), label: "Tee to-do lista", done: false},
         {id:uniqueId('todo-'), label: "Ota opintopisteet kotiin", done: false},
       ],
+      list: "todo-list",
+      summary: "todo-Ssummary"
     };
+  },
+  computed: {
+    listSummary() {
+      const numberFinishedItems = this.ToDoItems.filter((item) => item.done).length;
+      return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`;
+    },
   },
   methods: {
     addToDo(toDoLabel){
-      console.log("onSubmit method called");
-      console.log("to-do lisätty", toDoLabel);
+      /*console.log("onSubmit method called");
+      console.log("to-do lisätty", toDoLabel);*/
       this.ToDoItems.push({ id: uniqueId('todo-'), label: toDoLabel, done: false });
     },
   },
@@ -48,8 +57,8 @@ export default {
 <style>
 #app {
   font-family: Avenir, Arial, Helvetica, sans-serif;
-  -webkit-font-something: antialiased;
-  -moz-osx-font-something: grayscale;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
@@ -58,6 +67,50 @@ ul {
   list-style-type: none;
   padding: 0;
   margin: 0;
-}
-
+  margin-bottom: 0;
+  }
+  .stack-small > * + * {
+    margin-top: 1.25rem;
+  }
+  .stack-large > * + * {
+    margin-top: 2.5rem;
+  }
+  @media screen and (min-width: 550px) {
+    .stack-small > * + * {
+      margin-top: 1.4rem;
+    }
+    .stack-large > * + * {
+      margin-top: 2.8rem;
+    }
+  }
+  /* End global Styles */
+  #app {
+    background: #fff;
+    margin: 2rem 0 4rem 0;
+    padding: 1rem;
+    padding-top: 0;
+    position: relative;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+  }
+  @media screen and (min-width: 550px) {
+    #app {
+      padding: 4rem;
+    }
+  }
+  #app > * {
+    max-width: 50rem;
+    margin-left: car;
+    margin-right: auto;
+  }
+  #app > form {
+    max-width: 100%;
+  }
+  #app h1 {
+    display: block;
+    min-width: 100%;
+    width: 100%;
+    text-align: center;
+    margin: 0;
+    margin-bottom: 1rem;
+  }
 </style>
